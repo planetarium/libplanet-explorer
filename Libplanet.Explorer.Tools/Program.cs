88fcbcaa0b5505dc62a2daffa4b9d8be34674b86
Kidon Seo <kidonseo@gmail.com>
2021-04-01 19:12:35 +0900

    introduce mysql migration tool

---
+++ Libplanet.Explorer.Tools/Program.cs
@@ -0,0 +1,16 @@
+﻿using Cocona;
+using Libplanet.Explorer.Tools.SubCommand;
+
+namespace Libplanet.Explorer.Tools
+{
+    [HasSubCommands(typeof(MySqlStore), Description = "Manage MySql store.")]
+    class Program
+    {
+        static void Main(string[] args) => CoconaLiteApp.Run<Program>(args);
+
+        public void Help()
+        {
+            Main(new[] { "--help" });
+        }
+    }
+}
